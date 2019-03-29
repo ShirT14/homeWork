a19a68d6b30f21a295614287567b06e66974d601
ShirT14 <47032929+ShirT14@users.noreply.github.com>
2019-03-29 16:50:09 +0300

    Add files via upload

---
+++ beans/Customer.java
@@ -0,0 +1,93 @@
+package java.beans;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String emaile;
+	private String password;
+	private ArrayList <Coupon> coupons;
+	public Customer(int id, String firstName, String lastName, String emaile, String password,
+			ArrayList<Coupon> coupons) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emaile = emaile;
+		this.password = password;
+		this.coupons = coupons;
+	}
+	public Customer(int id) {
+		super();
+		this.id = id;
+	}
+	public Customer(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmaile() {
+		return emaile;
+	}
+	public void setEmaile(String emaile) {
+		this.emaile = emaile;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public ArrayList<Coupon> getCoupons() {
+		return coupons;
+	}
+	public void setCoupons(ArrayList<Coupon> coupons) {
+		this.coupons = coupons;
+	}
+	public int getId() {
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emaile=" + emaile
+				+ ", password=" + password + ", coupons=" + coupons + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+}
